3aa49a587a1163bd9447aff39dcdaade0e69f81f
subhasisghosal <supernovasubho@yahoo.in>
2019-05-28 01:03:42 +0530

    fixed minor flaw

---
+++ src/components/ListUser.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+
+  <section class="list-user">
+    <h1>list-user Component</h1>
+    <div>{{users}}</div>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'list-user',
+    props: [],
+    mounted() {
+      this.fetchUsers()
+    },
+    data() {
+      return {
+        users: []
+      }
+    },
+    methods: {
+      fetchUsers() {
+        const baseURI = 'https://shrouded-bastion-36522.herokuapp.com/admin/user/list/all'
+        this.$http.get(baseURI)
+        .then((result) => {
+          this.users = result.data
+        })
+      }
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.list-user {
+}
+</style>
